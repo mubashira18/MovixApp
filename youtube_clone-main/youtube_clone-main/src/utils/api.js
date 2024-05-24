77b69807aa2e0bc29128c099d64042bce95b8339
Mubashira31 <liferacer1718@gmail.com>
2024-05-24 13:05:57 +0530

    problems_fixed

---
+++ youtube_clone-main/youtube_clone-main/src/utils/api.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube138.p.rapidapi.com";
+
+const options = {
+  params: { hl: "en", gl: "US" },
+  headers: {
+    "X-RapidAPI-Key": "98d1badf0cmsh8cc410dc6633a9dp13fcc6jsnb76185a8c198",
+    "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+  },
+};
+
+export const fetchDataFromApi = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+  return data;
+};
